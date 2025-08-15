28ba5fffc1d65ab8a4a4db30876e0654ef10e643
Abdullah Hasan <AbdullahAbdelhady929@gmail.com>
2025-08-15 04:52:42 +0300

    q12

--- C44-G01-C#03/Program.cs
+++ C44-G01-C#03/Program.cs
@@ -101,6 +101,21 @@
             }
             #endregion
 
+            #region Question 12 - Identity Matrix
+            static void PrintIdentityMatrix(int n)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write((i == j) ? "1 " : "0 ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            #endregion
+
+
 
 
         }
